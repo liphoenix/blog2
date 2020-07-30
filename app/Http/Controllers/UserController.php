ad0e10ad638c23a6a1d2c562f3b934f4158e7f4d
1522168376@qq.com <1522168376@qq.com>
2020-07-30 10:39:51 +0800

    test

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view("user.add");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view("user.add");
+
+    }
+
+    /**
+     * 执行用户添加操作
+     * @param  提交的表单数据
+     * @return 返回添加是否成功
+     */
+    public function store(Request $request)
+    {
+        //1.获取客户端提交的表单数据
+//        $input = $request->all();
+        $input = $request->except('_token');
+//        dd($input);
+        $input['password'] = md5($input['password']);
+        //添加操作
+        $res = User::create($input);
+        dd($res);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
